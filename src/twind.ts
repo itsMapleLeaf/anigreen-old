e3614b9fb0d590f8cc75a4f1db9ba50ec05420b9
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-02-04 11:09:29 -0600

    more preflight tweaks

--- src/twind.ts
+++ src/twind.ts
@@ -1,19 +1,22 @@
 import { apply, setup } from "twind"
 import * as colors from "twind/colors"
-import { domSheet } from "twind/sheets"
+import { css } from "twind/css"
 
 setup({
 	darkMode: "class",
 	theme: {
-		colors: {
-			gray: colors.gray,
-		},
 		boxShadow: {
-			DEFAULT: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+			DEFAULT: "0px 3px 6px rgba(0, 0, 0, 0.2)",
+		},
+		extend: {
+			colors: {
+				gray: colors.gray,
+			},
 		},
 	},
 	preflight: {
-		body: apply`bg-gray-900 text-gray-100`,
+		body: css(apply`bg-gray-900 text-gray-100`, {
+			wordWrap: "break-word",
+		}),
 	},
-	sheet: domSheet(),
 })
