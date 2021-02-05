e297efad0fdb4eb69a2e42a29f46e3eee5cf1164
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-02-05 02:08:55 -0600

    error handling?

--- src/anilist.tsx
+++ src/anilist.tsx
@@ -1,16 +1,28 @@
-import { QueryKey, useQuery } from "react-query"
+import { QueryKey, useQuery, UseQueryOptions } from "react-query"
 import { GqlString } from "./gql"
 
-export function useAnilistQuery<Data>({
+class AnilistError extends Error {
+	errors
+
+	constructor(
+		status: number,
+		errors: readonly { status: number; message: string }[],
+	) {
+		super(`Api request failed with status ${status}`)
+		this.errors = errors
+	}
+}
+
+export function useAnilistQuery<Data, Variables = object>({
 	queryKey,
 	query,
 	variables,
 }: {
 	queryKey: QueryKey
 	query: GqlString
-	variables?: object
-}) {
-	return useQuery<{ data: Data }, unknown, Data>({
+	variables?: Variables
+} & UseQueryOptions<Data, unknown, Variables>) {
+	return useQuery<Data, unknown>({
 		queryKey: [queryKey, variables],
 		queryFn: async () => {
 			const response = await fetch(`/anilist`, {
@@ -25,8 +37,13 @@ export function useAnilistQuery<Data>({
 				}),
 			})
 
-			return response.json()
+			const json = await response.json()
+
+			if (!response.ok) {
+				throw new AnilistError(response.status, json.errors)
+			}
+
+			return json.data
 		},
-		select: (response) => response.data,
 	})
 }
