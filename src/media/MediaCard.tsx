7e000decb54e49cb596d0d01268ea159c42ad3bf
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-02-12 23:35:53 -0600

    ok time to stop fiddling with the card design

--- src/media/MediaCard.tsx
+++ src/media/MediaCard.tsx
@@ -11,26 +11,37 @@ export default function MediaCard({
 }) {
 	return (
 		<div
-			className={tw`relative rounded-lg overflow-hidden shadow bg-gray-800 flex flex-col`}
+			className={tw`relative overflow-hidden rounded-lg bg-black shadow flex flex-col`}
 		>
-			<Image
-				src={entry?.media?.bannerImage}
-				className={tw`w-full`}
-				style={{ aspectRatio: "2/1" }}
-			/>
+			<div className={tw`opacity-10`}>
+				<Image
+					src={entry?.media?.bannerImage}
+					className={tw`w-full h-full absolute`}
+				/>
+			</div>
 
-			<div className={tw`flex-1 flex py-4 pl-3 pr-2`}>
-				<div className={tw`flex flex-col flex-1`}>
+			<div className={tw`relative flex flex-1 pt-4 pb-3 pl-3 pr-2`}>
+				<div className={tw`flex flex-col flex-1 space-y-3`}>
 					<h3 className={tw`font-light text-2xl flex-1 -mt-2`}>
 						{entry?.media?.title?.userPreferred}
 					</h3>
-					<p>{entry?.progress || "No"} episodes watched</p>
-					<p>
-						{formatNextEpisode(
-							entry?.media?.nextAiringEpisode?.episode,
-							entry?.media?.nextAiringEpisode?.airingAt,
-						)}
-					</p>
+
+					<div className={tw`opacity-70 flex-1`}>
+						<p>{entry?.progress || "No"} episodes watched</p>
+						<p>
+							{formatNextEpisode(
+								entry?.media?.nextAiringEpisode?.episode,
+								entry?.media?.nextAiringEpisode?.airingAt,
+							)}
+						</p>
+					</div>
+
+					<button
+						type="button"
+						className={tw`opacity-50 hover:opacity-100 transition font-medium uppercase text-sm text-left py-1`}
+					>
+						Show More
+					</button>
 				</div>
 
 				<div>
