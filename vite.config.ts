2498c5afefed096202b3bd86e76b0a676d1f8aa8
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-02-14 10:11:28 -0600

    it's pwa time

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import reactRefresh from "@vitejs/plugin-react-refresh"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,9 +9,29 @@ export default defineConfig({
 		jsxFactory: `jsx.createElement`,
 		jsxFragment: `jsx.Fragment`,
 	},
-	plugins: [reactRefresh()],
 	clearScreen: false,
 	build: {
 		sourcemap: true,
 	},
+	plugins: [
+		reactRefresh(),
+		VitePWA({
+			manifest: {
+				icons: [
+					{
+						sizes: "192x192",
+						src: "/android-chrome-192x192.png",
+						type: "image/png",
+					},
+					{
+						sizes: "512x512",
+						src: "/android-chrome-512x512.png",
+						type: "image/png",
+					},
+				],
+				background_color: `#111827`,
+				theme_color: `#047857`,
+			},
+		}),
+	],
 })
