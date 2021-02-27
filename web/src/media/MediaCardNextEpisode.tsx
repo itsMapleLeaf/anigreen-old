2cc6fd1aeb5fed9c8ebb91c22062ef4ce90714b9
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-02-27 17:29:21 -0600

    MediaCardNextEpisode: cleanup

--- web/src/media/MediaCardNextEpisode.tsx
+++ web/src/media/MediaCardNextEpisode.tsx
@@ -10,37 +10,28 @@ export default function MediaCardNextEpisode({
 	entry: AnimeListEntryFragment
 }) {
 	const { airingAt, episode } = entry.media?.nextAiringEpisode ?? {}
-	if (airingAt && episode) {
-		return (
-			<Tooltip text={formatNextEpisodeExactDate(airingAt)}>
-				<p>
-					{formatNextEpisodeRelativeDate(episode, airingAt)}{" "}
-					<InfoIcon
-						className={tw`inline-block opacity-50`}
-						style={{ verticalAlign: "text-top" }}
-					/>
-				</p>
-			</Tooltip>
-		)
-	}
-	return null
-}
 
-function formatNextEpisodeRelativeDate(
-	episode?: number,
-	airingTimeSeconds?: number,
-) {
-	if (episode && airingTimeSeconds) {
-		return `Episode ${episode} airs ${relativeTime(airingTimeSeconds * 1000)}`
+	if (!(airingAt && episode)) {
+		return null
 	}
-}
 
-function formatNextEpisodeExactDate(airingTimeSeconds: number) {
-	return new Date(airingTimeSeconds * 1000).toLocaleString(undefined, {
+	const nextEpisodeDate = new Date(airingAt * 1000).toLocaleString(undefined, {
 		weekday: "long",
 		month: "long",
 		day: "numeric",
 		hour: "numeric",
 		minute: "numeric",
 	})
+
+	return (
+		<Tooltip text={nextEpisodeDate}>
+			<p>
+				{`Episode ${episode} airs ${relativeTime(airingAt * 1000)}`}{" "}
+				<InfoIcon
+					className={tw`inline-block opacity-50`}
+					style={{ verticalAlign: "text-top" }}
+				/>
+			</p>
+		</Tooltip>
+	)
 }
