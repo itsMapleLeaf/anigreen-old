4567d9cc8a5bdd9312e98ee54be0406bfc606fef
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-02-27 23:35:55 -0600

    disclosure: remove the other garbage

--- web/src/ui/disclosure.tsx
+++ web/src/ui/disclosure.tsx
@@ -1,5 +1,4 @@
-import constate from "constate"
-import { cloneElement, ReactElement, ReactNode, useState } from "react"
+import { useState } from "react"
 
 export function useDisclosure() {
 	const [isOpen, setOpen] = useState(false)
@@ -7,22 +6,4 @@ export function useDisclosure() {
 	const close = () => setOpen(false)
 	const toggle = () => setOpen((open) => !open)
 	return { isOpen, open, close, toggle }
-}
-
-export const [Disclosure, useDisclosureContext] = constate(useDisclosure)
-
-export function DisclosureButton({ children }: { children: ReactElement }) {
-	const context = useDisclosureContext()
-
-	return cloneElement(children, {
-		onClick: (...args: unknown[]) => {
-			context.toggle()
-			children.props?.onClick(...args)
-		},
-	})
-}
-
-export function DisclosureContent({ children }: { children: ReactNode }) {
-	const context = useDisclosureContext()
-	return <>{context.isOpen ? children : null}</>
 }
