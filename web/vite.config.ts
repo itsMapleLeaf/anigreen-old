7a85f9584806b1a7d895585e183ddec3e1ba3ea9
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-03-08 21:22:09 -0600

    remove pwa plugin
    trying to fix login issues on mobile, I figure that has something to do with the service worker?

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,6 @@
 import twindJsx from "@twind/vite-plugin-jsx"
 import reactRefresh from "@vitejs/plugin-react-refresh"
 import { defineConfig } from "vite"
-import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,27 +13,5 @@ export default defineConfig({
 	build: {
 		sourcemap: true,
 	},
-	plugins: [
-		reactRefresh(),
-		twindJsx(),
-		VitePWA({
-			manifest: {
-				start_url: "/",
-				icons: [
-					{
-						sizes: "192x192",
-						src: "/android-chrome-192x192.png",
-						type: "image/png",
-					},
-					{
-						sizes: "512x512",
-						src: "/android-chrome-512x512.png",
-						type: "image/png",
-					},
-				],
-				background_color: `#111827`,
-				theme_color: `#047857`,
-			},
-		}),
-	],
+	plugins: [reactRefresh(), twindJsx()],
 })
