dbdecf6b4f6616ce110e7a0357db0037bf980a21
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-13 23:15:28 -0500

    use preact alias

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,22 @@
+import preact from "@preact/preset-vite"
 import twindJsx from "@twind/vite-plugin-jsx"
-import reactRefresh from "@vitejs/plugin-react-refresh"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	esbuild: {
-		jsxInject: `import * as jsx from 'react'`,
-		jsxFactory: `jsx.createElement`,
-		jsxFragment: `jsx.Fragment`,
+	plugins: [preact(), twindJsx()],
+	resolve: {
+		alias: {
+			"react": "preact/compat",
+			"react-dom": "preact/compat",
+		},
+	},
+	// https://github.com/JoviDeCroock/prefresh/issues/321#issuecomment-819880115
+	optimizeDeps: {
+		exclude: [`@prefresh/vite/runtime`, `@prefresh/vite/utils`],
 	},
 	clearScreen: false,
 	build: {
 		sourcemap: true,
 	},
-	plugins: [reactRefresh(), twindJsx()],
 })
