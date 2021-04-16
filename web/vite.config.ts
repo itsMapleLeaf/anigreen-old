6e1674463a962e0f402208a0e4296d555ad116f0
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-16 00:57:43 -0500

    upgrades + remove preact, too many dev issues

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,18 +1,13 @@
-import preact from "@preact/preset-vite"
+import react from "@vitejs/plugin-react-refresh"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
-	resolve: {
-		alias: {
-			"react": "preact/compat",
-			"react-dom": "preact/compat",
-		},
-	},
-	// https://github.com/JoviDeCroock/prefresh/issues/321#issuecomment-819880115
-	optimizeDeps: {
-		exclude: [`@prefresh/vite/runtime`, `@prefresh/vite/utils`],
+	plugins: [react()],
+	esbuild: {
+		jsxInject: `import * as jsx from 'react'`,
+		jsxFactory: `jsx.createElement`,
+		jsxFragment: `jsx.Fragment`,
 	},
 	clearScreen: false,
 	build: {
