0379a7c567f5f778f3e7672cf6b01fedb5e249c7
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-17 16:02:45 -0500

    add "not found" page

--- web/src/app/AppRoutes.tsx
+++ web/src/app/AppRoutes.tsx
@@ -5,6 +5,16 @@ export default function AppRoutes() {
 	return (
 		<Routes>
 			<Route path="/watching" element={<WatchingPage />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
+	)
+}
+
+function NotFound() {
+	return (
+		<div className="grid gap-4 p-8">
+			<h1 className="text-3xl font-condensed">oops!</h1>
+			<p>{`couldn't find this page :(`}</p>
+		</div>
 	)
 }
