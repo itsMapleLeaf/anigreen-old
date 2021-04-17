7914f6be8044bd81adc1d271f84fe33915446217
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-17 18:47:33 -0500

    move some stuff

--- web/src/media/SchedulePage.tsx
+++ web/src/media/SchedulePage.tsx
@@ -1,8 +1,6 @@
-import { startOfWeek } from "date-fns"
 import { compact } from "lodash-es"
 import { useQuery } from "react-query"
 import { api } from "../api"
-import { MediaSeason } from "../generated/graphql"
 import { useViewerQuery } from "../viewer/queries"
 import { getMediaAiringDate } from "./getMediaAiringDate"
 import WeekdaySectionedList from "./WeekdaySectionedList"
@@ -21,8 +19,6 @@ export default function SchedulePage() {
 			compact(data.Page?.airingSchedules?.flatMap((item) => item?.media)),
 	})
 
-	console.log(startOfWeek(Date.now()))
-
 	return (
 		<WeekdaySectionedList
 			items={scheduleQuery.data ?? []}
@@ -35,12 +31,4 @@ export default function SchedulePage() {
 			)}
 		/>
 	)
-}
-
-function getCurrentMediaSeason() {
-	const month = new Date().getMonth()
-	if (month === 11 || month <= 1) return MediaSeason.Winter
-	if (month <= 4) return MediaSeason.Spring
-	if (month <= 7) return MediaSeason.Summer
-	return MediaSeason.Fall
 }
