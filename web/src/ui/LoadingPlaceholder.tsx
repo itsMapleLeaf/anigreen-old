29588c9e61d2566dd07dabefbe57c99066126353
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-17 16:03:17 -0500

    add more spacing to the loading placeholder

--- web/src/ui/LoadingPlaceholder.tsx
+++ web/src/ui/LoadingPlaceholder.tsx
@@ -2,7 +2,7 @@ import LoadingIcon from "./LoadingIcon"
 
 export default function LoadingPlaceholder() {
 	return (
-		<div className="grid justify-items-center p-4">
+		<div className="grid h-64 place-content-center">
 			<LoadingIcon className="w-12 h-12" />
 		</div>
 	)
