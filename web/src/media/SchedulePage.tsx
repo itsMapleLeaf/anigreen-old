a183b9fa9c44c2d628294026559150a0c9d2bfc4
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-18 16:18:05 -0500

    SchedulePage: remove auth requirement

--- web/src/media/SchedulePage.tsx
+++ web/src/media/SchedulePage.tsx
@@ -1,21 +1,16 @@
 import { compact } from "lodash-es"
 import { useQuery } from "react-query"
 import { api } from "../api"
-import { useViewerQuery } from "../viewer/queries"
 import { getMediaAiringDate } from "./getMediaAiringDate"
 import MediaCard from "./MediaCard"
 import WeekdaySectionedList from "./WeekdaySectionedList"
 
 export default function SchedulePage() {
-	const viewerQuery = useViewerQuery({ required: true })
-	const userId = viewerQuery.data?.Viewer?.id
-
 	const scheduleQuery = useQuery({
 		queryKey: ["schedule"],
 		queryFn: () => {
 			return api.Schedule({ todaySeconds: Math.floor(Date.now() / 1000) })
 		},
-		enabled: !!userId,
 		select: (data) =>
 			compact(data.Page?.airingSchedules?.flatMap((item) => item?.media)),
 	})
