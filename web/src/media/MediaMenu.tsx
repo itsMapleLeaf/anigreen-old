97fb6895ec8672aaf0a0a49b9e90c33b460180ab
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-18 17:48:54 -0500

    show link domains for links with same names

--- web/src/media/MediaMenu.tsx
+++ web/src/media/MediaMenu.tsx
@@ -2,12 +2,23 @@ import { DotsVerticalIcon, ExternalLinkIcon } from "@heroicons/react/solid"
 import { compact } from "lodash-es"
 import Button from "../dom/Button"
 import ExternalLink from "../dom/ExternalLink"
-import type { MediaFragment } from "../generated/graphql"
+import type {
+	MediaExternalLinkFragment,
+	MediaFragment,
+} from "../generated/graphql"
 import { clearIconButtonStyle } from "../ui/components"
 import IconWithText from "../ui/IconWithText"
 import { Menu, MenuButton, MenuItem, MenuPanel } from "../ui/menu"
 
 export default function MediaMenu({ media }: { media: MediaFragment }) {
+	const externalLinks = compact(media.externalLinks)
+
+	function otherLinkExistsWithSameName(link: MediaExternalLinkFragment) {
+		return externalLinks.some(
+			(other) => other.id !== link.id && other.site === link.site,
+		)
+	}
+
 	return (
 		<Menu>
 			<MenuButton>
@@ -16,13 +27,17 @@ export default function MediaMenu({ media }: { media: MediaFragment }) {
 				</Button>
 			</MenuButton>
 			<MenuPanel>
-				{compact(media.externalLinks)?.map((link) => (
+				{externalLinks.map((link) => (
 					<MenuItem key={link.id}>
 						<ExternalLink href={link.url}>
-							<IconWithText
-								iconLeft={<ExternalLinkIcon className="w-5" />}
-								text={link.site}
-							/>
+							<IconWithText iconLeft={<ExternalLinkIcon className="w-5" />}>
+								<p>{link.site}</p>
+								{otherLinkExistsWithSameName(link) && (
+									<p className="text-sm leading-tight opacity-75">
+										{getDomain(link.url)}
+									</p>
+								)}
+							</IconWithText>
 						</ExternalLink>
 					</MenuItem>
 				))}
@@ -37,4 +52,13 @@ export default function MediaMenu({ media }: { media: MediaFragment }) {
 			</MenuPanel>
 		</Menu>
 	)
+}
+
+function getDomain(urlString: string) {
+	try {
+		// looks prettier without www I guess
+		return new URL(urlString).hostname.replace(/^www\./, "")
+	} catch {
+		return ""
+	}
 }
