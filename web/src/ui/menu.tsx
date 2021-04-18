21f383bf573dd37048232d450d2dd11656e36c00
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-17 23:37:43 -0500

    add popper to menu

--- web/src/ui/menu.tsx
+++ web/src/ui/menu.tsx
@@ -1,15 +1,19 @@
-import { Menu as BaseMenu, Transition } from "@headlessui/react"
+import { Menu as BaseMenu, Portal, Transition } from "@headlessui/react"
 import * as React from "react"
 import Slot from "../react/Slot"
+import { createPopperContext } from "./popper"
 
 const OpenContext = React.createContext(false)
+const MenuPopper = createPopperContext()
 
 export function Menu({ children }: { children: React.ReactNode }) {
 	return (
 		<BaseMenu>
 			{({ open }) => (
 				<OpenContext.Provider value={open}>
-					<div className="relative">{children}</div>
+					<MenuPopper.Root>
+						<div className="relative">{children}</div>
+					</MenuPopper.Root>
 				</OpenContext.Provider>
 			)}
 		</BaseMenu>
@@ -17,28 +21,38 @@ export function Menu({ children }: { children: React.ReactNode }) {
 }
 
 export function MenuButton({ children }: { children: React.ReactElement }) {
-	return <BaseMenu.Button as={React.Fragment}>{children}</BaseMenu.Button>
+	return (
+		<MenuPopper.Reference>
+			<BaseMenu.Button as={React.Fragment}>{children}</BaseMenu.Button>
+		</MenuPopper.Reference>
+	)
 }
 
 export function MenuPanel({ children }: { children: React.ReactNode }) {
 	const open = React.useContext(OpenContext)
 	return (
-		<Transition
-			show={open}
-			enter="transition duration-100"
-			enterFrom="transform scale-95 opacity-0"
-			enterTo="transform scale-100 opacity-100"
-			leave="transition duration-100"
-			leaveFrom="transform scale-100 opacity-100"
-			leaveTo="transform scale-95 opacity-0"
-		>
-			<BaseMenu.Items
-				static
-				className="absolute right-0 mt-2 overflow-hidden text-gray-800 bg-white rounded shadow min-w-[10rem] top-full"
-			>
-				{children}
-			</BaseMenu.Items>
-		</Transition>
+		<Transition.Root show={open}>
+			<Portal>
+				<MenuPopper.Element>
+					<Transition.Child
+						as={React.Fragment}
+						enter="transition duration-100"
+						enterFrom="transform scale-95 opacity-0"
+						enterTo="transform scale-100 opacity-100"
+						leave="transition duration-100"
+						leaveFrom="transform scale-100 opacity-100"
+						leaveTo="transform scale-95 opacity-0"
+					>
+						<BaseMenu.Items
+							static
+							className="text-gray-800 bg-white rounded shadow min-w-[10rem]"
+						>
+							{children}
+						</BaseMenu.Items>
+					</Transition.Child>
+				</MenuPopper.Element>
+			</Portal>
+		</Transition.Root>
 	)
 }
 
