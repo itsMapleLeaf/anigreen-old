4ed63715d6ceb9036d5ad9ccc423d50049671271
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-23 19:06:06 -0500

    MediaCard: accept children

--- web/src/media/MediaCard.tsx
+++ web/src/media/MediaCard.tsx
@@ -1,8 +1,14 @@
+import type { ReactNode } from "react"
 import type { MediaFragment } from "../generated/graphql"
 import MediaCardBanner from "./MediaCardBanner"
-import MediaNextEipsode from "./MediaNextEipsode"
 
-export default function MediaCard({ media }: { media: MediaFragment }) {
+export default function MediaCard({
+	media,
+	children,
+}: {
+	media: MediaFragment
+	children: ReactNode
+}) {
 	return (
 		<div className="relative flex flex-col overflow-hidden rounded-lg shadow">
 			<MediaCardBanner media={media} />
@@ -11,10 +17,7 @@ export default function MediaCard({ media }: { media: MediaFragment }) {
 					<h3 className="flex-1 text-xl font-light font-condensed">
 						{media.title?.userPreferred}
 					</h3>
-					<div className="flex-1" />
-					<div className="pr-6 mt-2 opacity-70">
-						<MediaNextEipsode media={media} />
-					</div>
+					{children}
 				</div>
 			</div>
 		</div>
