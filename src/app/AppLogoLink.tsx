89a0f55504b931cb7cf668b625acc36262f2c049
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-28 22:26:23 -0500

    AppLogoLink: use client link

--- src/app/AppLogoLink.tsx
+++ src/app/AppLogoLink.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+
 export default function AppLogoLink() {
 	return (
-		<a href="/" className="space-y-1">
+		<Link to="/" className="space-y-1">
 			<h1 className="text-3xl leading-none">
 				<span className="text-blue-300">ani</span>
 				<span className="text-green-300">green</span>
 			</h1>
 			<p className="text-sm opacity-50">name pending</p>
-		</a>
+		</Link>
 	)
 }
