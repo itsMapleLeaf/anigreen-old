1bd325185b10572a2d54f57ef8e4a1f32551b0ab
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-04-28 22:26:36 -0500

    client redirect to /watching

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom"
+import { useEffect } from "react"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import SchedulePage from "../media/SchedulePage"
 import WatchingPage from "../media/WatchingPage"
 
 export default function AppRoutes() {
 	return (
 		<Routes>
+			<Route path="/" element={<Redirect to="/watching" />} />
 			<Route path="/watching" element={<WatchingPage />} />
 			<Route path="/schedule" element={<SchedulePage />} />
 			<Route path="*" element={<NotFound />} />
@@ -19,4 +21,10 @@ function NotFound() {
 			<p>{`couldn't find this page :(`}</p>
 		</div>
 	)
+}
+
+function Redirect({ to }: { to: string }) {
+	const navigate = useNavigate()
+	useEffect(() => navigate(to), [navigate, to])
+	return null
 }
