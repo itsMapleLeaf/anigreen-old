0552c5ece908abb63fef48dcfe33c1472e87187d
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-05-08 17:15:13 -0500

    create an express app and export that directly
    instead of the roundabout Router nonsense

--- server/handler.ts
+++ server/handler.ts
@@ -2,115 +2,111 @@ import assert from "assert"
 import Axios from "axios"
 import cookieSession from "cookie-session"
 import "dotenv/config"
-import express, { ErrorRequestHandler, Router } from "express"
+import express, { ErrorRequestHandler } from "express"
 import proxy from "express-http-proxy"
 import morgan from "morgan"
 
 const oneDayMs = 1000 * 60 * 60 * 24
 
-function createHandler() {
-	const handler = Router()
+const app = express()
 
-	handler.use(morgan("short"))
+app.use(morgan("short"))
 
-	handler.use(
-		cookieSession({
-			name: "session",
-			secret: process.env["COOKIE_SECRET"] as string,
-			httpOnly: true,
-			expires: new Date(Date.now() + oneDayMs * 30),
-		}),
-	)
+app.use(
+	cookieSession({
+		name: "session",
+		secret: process.env["COOKIE_SECRET"] as string,
+		httpOnly: true,
+		expires: new Date(Date.now() + oneDayMs * 30),
+	}),
+)
 
-	handler.use((req, res, next) => {
-		assert(req.session)
+app.use((req, res, next) => {
+	assert(req.session)
 
-		console.log(req.headers)
+	console.log(req.headers)
 
-		if (req.session.user && Date.now() > req.session.user.expiresAt) {
-			req.session.user = undefined
-		}
-		next()
-	})
-
-	// https://expressjs.com/en/resources/middleware/cookie-session.html#extending-the-session-expiration
-	handler.use((req, res, next) => {
-		assert(req.session)
-		req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
-		next()
-	})
-
-	handler.get("/auth-redirect", async (req, res) => {
-		assert(req.session)
-
-		const response = await Axios.post("https://anilist.co/api/v2/oauth/token", {
-			grant_type: "authorization_code",
-			client_id: process.env["ANILIST_APP_ID"],
-			client_secret: process.env["ANILIST_APP_SECRET"],
-			redirect_uri: process.env["ANILIST_REDIRECT_URL"],
-			code: req.query.code,
-		})
-
-		req.session.user = {
-			...response.data,
-			expiresAt: Date.now() + response.data.expires_in * 1000,
-		}
-
-		res.redirect("/")
-	})
-
-	handler.get("/login", (req, res) => {
-		res.redirect(
-			`https://anilist.co/api/v2/oauth/authorize` +
-				`?client_id=${process.env["ANILIST_APP_ID"]}` +
-				`&redirect_uri=${process.env["ANILIST_REDIRECT_URL"]}` +
-				`&response_type=code`,
-		)
-	})
-
-	handler.get("/logout", (req, res) => {
-		assert(req.session)
-
+	if (req.session.user && Date.now() > req.session.user.expiresAt) {
 		req.session.user = undefined
-		res.redirect("/")
-	})
-
-	handler.use(
-		"/anilist",
-		proxy(`https://graphql.anilist.co`, {
-			proxyReqOptDecorator(options, req) {
-				if (!req.session!.user) {
-					return options
-				}
-
-				return {
-					...options,
-					headers: {
-						...options.headers,
-						Authorization: `Bearer ${req.session!.user.access_token}`,
-					},
-				}
-			},
-		}),
-	)
-
-	handler.get("/", (req, res) => {
-		assert(req.session)
-
-		if (req.session.user) {
-			res.redirect("/watching")
-		} else {
-			res.redirect("/schedule")
-		}
-	})
-
-	const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-		console.error(err.stack)
-		res.status(500).send(err.message ?? "An internal error occurred")
 	}
-	handler.use(errorHandler)
+	next()
+})
 
-	return handler
+// https://expressjs.com/en/resources/middleware/cookie-session.html#extending-the-session-expiration
+app.use((req, res, next) => {
+	assert(req.session)
+	req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
+	next()
+})
+
+app.get("/auth-redirect", async (req, res) => {
+	assert(req.session)
+
+	const response = await Axios.post("https://anilist.co/api/v2/oauth/token", {
+		grant_type: "authorization_code",
+		client_id: process.env["ANILIST_APP_ID"],
+		client_secret: process.env["ANILIST_APP_SECRET"],
+		redirect_uri: process.env["ANILIST_REDIRECT_URL"],
+		code: req.query.code,
+	})
+
+	req.session.user = {
+		...response.data,
+		expiresAt: Date.now() + response.data.expires_in * 1000,
+	}
+
+	res.redirect("/")
+})
+
+app.get("/login", (req, res) => {
+	res.redirect(
+		`https://anilist.co/api/v2/oauth/authorize` +
+			`?client_id=${process.env["ANILIST_APP_ID"]}` +
+			`&redirect_uri=${process.env["ANILIST_REDIRECT_URL"]}` +
+			`&response_type=code`,
+	)
+})
+
+app.get("/logout", (req, res) => {
+	assert(req.session)
+
+	req.session.user = undefined
+	res.redirect("/")
+})
+
+app.use(
+	"/anilist",
+	proxy(`https://graphql.anilist.co`, {
+		proxyReqOptDecorator(options, req) {
+			if (!req.session!.user) {
+				return options
+			}
+
+			return {
+				...options,
+				headers: {
+					...options.headers,
+					Authorization: `Bearer ${req.session!.user.access_token}`,
+				},
+			}
+		},
+	}),
+)
+
+app.get("/", (req, res) => {
+	assert(req.session)
+
+	if (req.session.user) {
+		res.redirect("/watching")
+	} else {
+		res.redirect("/schedule")
+	}
+})
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+	console.error(err.stack)
+	res.status(500).send(err.message ?? "An internal error occurred")
 }
+app.use(errorHandler)
 
-export const handler = express().use(createHandler())
+export { app as handler }
