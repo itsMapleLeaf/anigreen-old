1bf4d9b54e076729ec563a4ee67cba4b0600811d
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-05-09 17:53:15 -0500

    cleaner build output folders
    build folders being in public made me uncomfortable

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  browserBuildDirectory: "build/browser",
+  serverBuildDirectory: "build/server",
   appDirectory: "app",
-  browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002
-};
+  devServerPort: 8002,
+}
