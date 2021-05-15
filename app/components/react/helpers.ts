5d078eab1c9ea75117aa376b6254321c04a8941c
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-05-15 18:29:49 -0500

    more permissive autoRef signature

--- app/components/react/helpers.ts
+++ app/components/react/helpers.ts
@@ -3,11 +3,12 @@ import { forwardRef, ReactElement, Ref } from "react"
 /**
  * Passes ref as a normal prop, makes `forwardRef`ing easier in some cases
  */
-export function autoRef<Props extends { ref?: RefType }, RefType>(fc: {
-	(props: Props): ReactElement | null
-	displayName?: string
+export function autoRef<Props, RefType>(fc: {
+  (props: Props): ReactElement | null
+  displayName?: string
 }) {
-	const AutoRef = (props: Props, ref: Ref<RefType>) => fc({ ...props, ref })
-	AutoRef.displayName = fc.displayName || fc.name || "AutoRef"
-	return forwardRef(AutoRef)
+  const AutoRef = (props: Props & { ref?: RefType }, ref: Ref<RefType>) =>
+    fc({ ...props, ref })
+  AutoRef.displayName = fc.displayName || fc.name || "AutoRef"
+  return forwardRef(AutoRef)
 }
