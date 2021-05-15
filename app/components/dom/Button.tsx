30db3dc539e4fa82f70a9a2fec4e750faf179f43
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-05-15 18:31:38 -0500

    export button props

--- app/components/dom/Button.tsx
+++ app/components/dom/Button.tsx
@@ -1,25 +1,25 @@
-import type { ComponentProps } from "react"
+import type { ComponentPropsWithoutRef } from "react"
 import { autoRef } from "../react/helpers"
 import LoadingIcon from "../ui/LoadingIcon"
 
-type Props = ComponentProps<"button"> & {
-	loading?: boolean
+export type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  loading?: boolean
 }
 
 export default autoRef(function Button({
-	loading,
-	children,
-	className = "",
-	...props
-}: Props) {
-	return (
-		<button
-			type="button"
-			disabled={loading}
-			className={`${loading ? `opacity-50` : ""} ${className}`}
-			{...props}
-		>
-			{loading ? <LoadingIcon className="w-5" /> : children}
-		</button>
-	)
+  loading,
+  children,
+  className = "",
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      className={`${loading ? `opacity-50` : ""} ${className}`}
+      {...props}
+    >
+      {loading ? <LoadingIcon className="w-5" /> : children}
+    </button>
+  )
 })
